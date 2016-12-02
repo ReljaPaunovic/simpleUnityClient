e5ab53b3e7c1f85830adbdd6acb02ec57dcbd9d9
Relja Paunovic <relja.2.paunovic@aalto.fi>
2016-12-03 01:04:21 +0200

    whole thing

---
+++ Assets/handleEvent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems; // Required when using event data
+using UnityEngine.UI;
+
+public class handleEvent : MonoBehaviour, IEndDragHandler
+{
+    public ScrollRect scroll;
+    private int index = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //Do this when the user stops dragging this UI Element.
+    public void OnEndDrag(PointerEventData data)
+    {
+        // If reached the end of list
+        if( scroll.normalizedPosition.y == 0.0f){
+            var names = GameObject.Find("Camera").GetComponent<main>().names;
+            if (names.Count >= index + 10)
+            {
+                for (var i = index; i < index + 10; i++)
+                {
+                    Transform txt = (Transform)Instantiate(GameObject.Find("Camera").GetComponent<main>().listElement, 
+                                                            GameObject.Find("Camera").GetComponent<main>().parrent);
+                    txt.GetComponent<Text>().text = names[i];
+                }
+                index = index + 10;
+            }
+            else
+            {
+                for (var i = index; i < names.Count; i++)
+                {
+                    Transform txt = (Transform)Instantiate(GameObject.Find("Camera").GetComponent<main>().listElement,
+                                                            GameObject.Find("Camera").GetComponent<main>().parrent);
+                    txt.GetComponent<Text>().text = names[i];
+                }
+                index = names.Count;
+            }
+        }
+    }
+}
---
+++ Assets/main.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using SimpleJSON;
+using System.Collections.Generic;
+
+public class main : MonoBehaviour
+{
+
+    public Text queryField;
+    public List<string> names = new List<string>();
+    public Transform listElement;
+    public Transform parrent;
+
+    void showFirstTen()
+    {
+        if (names.Count >= 10)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Transform txt = (Transform)Instantiate(listElement, parrent);
+                txt.GetComponent<Text>().text = names[i];
+            }
+        }
+        // Just in case there are less than 10 results
+        else
+        {
+            for (var i = 0; i < names.Count; i++)
+            {
+                Transform txt = (Transform)Instantiate(listElement, parrent);
+                txt.GetComponent<Text>().text = names[i];
+
+            }
+        }
+    }
+    public void doSomething()
+    {
+        // DESTROY ALL CHILDREN!!
+        int childs = parrent.childCount;
+        for (int i = childs - 1; i >= 0; i--)
+        {
+            GameObject.Destroy(parrent.GetChild(i).gameObject);
+        }
+        names.Clear();
+        // I LIMITED IT TO 100 RESULTS, DEFAULT IS 25
+
+        string query = queryField.text.ToString();
+        string url = "https://external.api.yle.fi/v1/programs/items.json?app_id=b8873981&app_key=47bf6d45f9a552889a679154849b1023&language=fi&limit=100&q=" + query;
+        WWW www = new WWW(url);
+        StartCoroutine(WaitForRequest(www));
+    }
+
+    IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            var data = JSON.Parse(www.text);
+            var n = data["data"].Count;
+            for (var i = 0; i < n; i++)
+            {
+                names.Add(data["data"][i]["title"]["fi"]);
+            }
+            showFirstTen();
+        }
+        else {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+}
